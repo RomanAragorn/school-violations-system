9746a5b4505f5083f32a5e11a7849f712632ce9d
Xenon <joshua123.jdr@gmail.com>
2025-12-28 23:59:55 +0800

    feat: implement middleware for checking admin

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,25 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('show.login');
+});
+
+// Login
+Route::group(['middleware' => 'guest'], function () {
+    Route::get('/login', [AuthController::class, 'showLogin'])->name('show.login');
+    Route::post('login', [AuthController::class, 'login'])->name('login');
+});
+
+Route::group(['middleware' => 'auth'], function () {
+
+    Route::get('/admin/dashboard', function () {
+        return view('admin.dashboard');
+    })->middleware('is_admin');
+
+    Route::get('/user/dashboard', function () {
+        return view('user.dashboard');
+    });
 });
