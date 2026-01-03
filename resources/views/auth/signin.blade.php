b0d41c9fc121e72d67e20f275c8686fe6b0a1d5c
Xenon <joshua123.jdr@gmail.com>
2026-01-03 14:47:36 +0800

    chore: use bootstrap icons package instead of external link

--- resources/views/auth/signin.blade.php
+++ resources/views/auth/signin.blade.php
@@ -6,7 +6,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
 </head>
 
 <body class="d-flex flex-column min-vh-100">
